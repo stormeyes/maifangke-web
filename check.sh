e307292e8deaf374b408037e6c6b58e2eeb361ac
kongkongyzt <stormeyes@users.noreply.github.com>
2020-06-22 16:12:55 +0800

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-git clone --depth=1 https://github.com/stormeyes/maifangke-web
+git clone https://github.com/stormeyes/maifangke-web
 cd maifangke-web
-ls
-git branch | xargs echo
-echo $1
-git checkout $1
-git branch | xargs echo
+found_secret_id=`git diff master..origin/$1 | grep -E '[^a-zA-Z0-9](AKID[a-zA-Z0-9]{32})([^a-zA-Z0-9]|$)'`	
+if [ -n "$found_secret_id" ] ; then	
+  echo 11111 $found_secret_id	
+  exit 1	
+fi

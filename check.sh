9e22110802797457b2c6d7951805ae5f6ab090c7
kongkongyzt <stormeyes@users.noreply.github.com>
2020-06-22 15:44:36 +0800

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-FILTER_RESULT=`git diff --name-only HEAD~1 | xargs grep "[^a-zA-Z0-9](AKID[a-zA-Z0-9]{32})([^a-zA-Z0-9]|$) AND [^a-zA-Z0-9]([a-zA-Z0-9]{32})([^a-zA-Z0-9]|$)"`
-if [ -n "$FILTER_RESULT" ]; then
-  git reset --hard HEAD~1
-fi
+git clone --depth=1 https://github.com/stormeyes/maifangke-web
+cd maifangke-web
+ls
